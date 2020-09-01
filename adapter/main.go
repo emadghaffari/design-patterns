3d17f4291d53f618b1b3388dcf7b23728d466067
Emad <emadghaffariii@gmail.com>
2020-09-01 14:45:55 +0000

    add main file to adapter

--- adapter/main.go
+++ adapter/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/Emadghaffari/design-patterns/adapter/packages/new"
+
+	"github.com/Emadghaffari/design-patterns/adapter/packages/old"
+)
+
+func main() {
+	oldPrinter := old.MyPrinter{}
+	oldMsg := oldPrinter.Print("The old message")
+	fmt.Println(oldMsg)
+
+	newPrinter := new.MyPrinter{"new Message from old Printer", oldPrinter}
+	newMsg := newPrinter.Print()
+	fmt.Println(newMsg)
 }
